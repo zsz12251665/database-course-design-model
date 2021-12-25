9a027fc0108aa1bd050a9d251bbc9f85fe39c077
zsz12251665 <zsz12251665@users.noreply.github.com>
2021-12-26 00:12:56 +0800

    Add MYSQL_PORT to env variables

--- src/query/base.ts
+++ src/query/base.ts
@@ -2,6 +2,7 @@ import mysql from 'mysql';
 
 const pool = mysql.createPool({
 	host: process.env.MYSQL_HOST,
+	port: Number(process.env.MYSQL_PORT ?? 3306),
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE
